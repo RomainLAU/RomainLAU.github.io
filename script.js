bea4c9f6922ff76c816fda8b44a0051581a4c3ff
RomainLAU <romain.laurent@epitech.digital>
2022-03-05 02:48:41 +0100

    Formulaire de contact terminé et ajout d'un reset css

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     let lastFirstname = document.querySelector('#lastFirstname')
-    let lastFirstnamePlaceHolder = document.querySelector('#lastFirstname')[0].placeholder
     let email = document.querySelector('#email')
-    let mailObject = document.querySelector('#mailObject')
-    let mailContent = document.querySelector('#mailContent')
+    let mailObject = document.querySelector('#contact__title-input')
+    let mailContent = document.querySelector('#contact__comment-input')
     let submitButton = document.querySelector('#submit')
 
-    let lastFirstnameError = document.querySelector('#lastFirstnameError')
-    let emailError = document.querySelector('#emailError')
-    let mailObjectError = document.querySelector('#mailObjectError')
-    let mailContentError = document.querySelector('#mailContentError')
+    let emailRegex = /[^\s@]+@[^\s@]+\.[^\s@]{2,}/
+
+    let formIsIncorrect = 0
 
     submitButton.addEventListener('click', function(event) {
-        event.preventDefault()
 
         if (lastFirstname.value.length === 0) {
-            lastFirstnamePlaceHolder = "Veuillez remplir ce champ."
-            lastFirstnamePlaceHolder.style.setProperty("red")
+            lastFirstname.style.border = "solid 2px red"
+            formIsIncorrect += 1
+        } else {
+            lastFirstname.style.border = "none"
+        }
+
+        if (!email || email.value.length === 0 || !(email.value.match(emailRegex))) {
+            email.style.border = "solid 2px red"
+            formIsIncorrect += 1
+        } else {
+            email.style.border = "none"
+        }
+
+        if (!mailObject || mailObject.value.length === 0) {
+            mailObject.style.border = "solid 2px red"
+            formIsIncorrect += 1
+        } else {
+            mailObject.style.border = "none"
+        }
+
+        if (!mailContent || mailContent.value.length === 0) {
+            mailContent.style.border = "solid 2px red"
+            formIsIncorrect += 1
+        } else {
+            mailContent.style.border = "none"
+        }
+
+        if (formIsIncorrect === 4) {
+            event.preventDefault()
         }
     })
 })
