4ab423b10da144587866187a396117bf1f95afa2
RomainLAU <romain.laurent@epitech.digital>
2022-03-15 01:12:44 +0100

    Correcting wrong action on the form, the script was letting the form to be posted as soon as one input was good

--- script.js
+++ script.js
@@ -8,40 +8,34 @@ window.addEventListener('DOMContentLoaded', function() {
 
     let emailRegex = /[^\s@]+@[^\s@]+\.[^\s@]{2,}/
 
-    let formIsIncorrect = 0
-
     submitButton.addEventListener('click', function(event) {
 
         if (lastFirstname.value.length === 0) {
             lastFirstname.style.border = "solid 2px red"
-            formIsIncorrect += 1
+            event.preventDefault()
         } else {
             lastFirstname.style.border = "none"
         }
 
         if (!email || email.value.length === 0 || !(email.value.match(emailRegex))) {
             email.style.border = "solid 2px red"
-            formIsIncorrect += 1
+            event.preventDefault()
         } else {
             email.style.border = "none"
         }
 
         if (!mailObject || mailObject.value.length === 0) {
             mailObject.style.border = "solid 2px red"
-            formIsIncorrect += 1
+            event.preventDefault()
         } else {
             mailObject.style.border = "none"
         }
 
         if (!mailContent || mailContent.value.length === 0) {
             mailContent.style.border = "solid 2px red"
-            formIsIncorrect += 1
+            event.preventDefault()
         } else {
             mailContent.style.border = "none"
-        }
-
-        if (formIsIncorrect === 4) {
-            event.preventDefault()
         }
     })
 })
