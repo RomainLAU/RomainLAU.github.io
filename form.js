e5499d0475f6185f51863029ad0778628f63f955
RomainLAU <romain.laurent@epitech.digital>
2022-10-04 15:21:14 +0200

    Remove console.log

--- form.js
+++ form.js
@@ -1,41 +1,46 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
+  let lastFirstname = document.querySelector("#lastFirstname");
+  let email = document.querySelector("#email");
+  let mailObject = document.querySelector("#contact__title-input");
+  let mailContent = document.querySelector("#contact__comment-input");
+  let submitButton = document.querySelector("#submit");
 
-    let lastFirstname = document.querySelector('#lastFirstname')
-    let email = document.querySelector('#email')
-    let mailObject = document.querySelector('#contact__title-input')
-    let mailContent = document.querySelector('#contact__comment-input')
-    let submitButton = document.querySelector('#submit')
+  let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-    let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+  submitButton.addEventListener("click", (event) => {
+    if (
+      lastFirstname.value.length === 0 &&
+      parseInt(lastFirstname.value) != NaN
+    ) {
+      lastFirstname.style.borderColor = "red";
+      event.preventDefault();
+    } else {
+      lastFirstname.style.borderColor = "rgb(255, 174, 0)";
+    }
 
-    submitButton.addEventListener('click', (event) => {
+    if (
+      !email.value ||
+      email.value.length === 0 ||
+      email.value.match(emailRegex) === null
+    ) {
+      email.style.borderColor = "red";
+      event.preventDefault();
+    } else {
+      email.style.borderColor = "rgb(255, 174, 0)";
+    }
 
-        if (lastFirstname.value.length === 0) {
-            lastFirstname.style.border = "solid 2px red"
-            event.preventDefault()
-        } else {
-            lastFirstname.style.border = "none"
-        }
+    if (!mailObject || mailObject.value.length === 0) {
+      mailObject.style.borderColor = "red";
+      event.preventDefault();
+    } else {
+      mailObject.style.borderColor = "rgb(255, 174, 0)";
+    }
 
-        if (!email.value || email.value.length === 0 || email.value.match(emailRegex) === null) {
-            email.style.border = "solid 2px red"
-            event.preventDefault()
-        } else {
-            email.style.border = "none"
-        }
-
-        if (!mailObject || mailObject.value.length === 0) {
-            mailObject.style.border = "solid 2px red"
-            event.preventDefault()
-        } else {
-            mailObject.style.border = "none"
-        }
-
-        if (!mailContent || mailContent.value.length === 0) {
-            mailContent.style.border = "solid 2px red"
-            event.preventDefault()
-        } else {
-            mailContent.style.border = "none"
-        }
-    })
-})
+    if (!mailContent || mailContent.value.length === 0) {
+      mailContent.style.borderColor = "red";
+      event.preventDefault();
+    } else {
+      mailContent.style.borderColor = "rgb(255, 174, 0)";
+    }
+  });
+});
