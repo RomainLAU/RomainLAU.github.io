731d9e484ec0e7af1fffbf2a9a0d30ffe3040bf3
RomainLAU <romain.laurent@epitech.digital>
2022-11-25 00:34:26 +0100

    Change correct color inputs

--- form.js
+++ form.js
@@ -1,23 +1,23 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const lastFirstname = document.querySelector("#lastFirstname");
-  const email = document.querySelector("#email");
-  const mailObject = document.querySelector("#contact__title-input");
-  const mailContent = document.querySelector("#contact__comment-input");
-  const submitButton = document.querySelector("#submit");
+window.addEventListener('DOMContentLoaded', () => {
+  const lastFirstname = document.querySelector('#lastFirstname');
+  const email = document.querySelector('#email');
+  const mailObject = document.querySelector('#contact__title-input');
+  const mailContent = document.querySelector('#contact__comment-input');
+  const submitButton = document.querySelector('#submit');
 
   const nameRegex =
     /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
   const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-  submitButton.addEventListener("click", (event) => {
+  submitButton.addEventListener('click', (event) => {
     if (
       lastFirstname.value.length === 0 ||
       !lastFirstname.value.match(nameRegex)
     ) {
-      lastFirstname.style.borderColor = "red";
+      lastFirstname.style.borderColor = 'red';
       event.preventDefault();
     } else {
-      lastFirstname.style.borderColor = "rgb(255, 174, 0)";
+      lastFirstname.style.borderColor = '#529d44';
     }
 
     if (
@@ -25,24 +25,24 @@ window.addEventListener("DOMContentLoaded", () => {
       email.value.length === 0 ||
       email.value.match(emailRegex) === null
     ) {
-      email.style.borderColor = "red";
+      email.style.borderColor = 'red';
       event.preventDefault();
     } else {
-      email.style.borderColor = "rgb(255, 174, 0)";
+      email.style.borderColor = '#529d44';
     }
 
     if (!mailObject || mailObject.value.length === 0) {
-      mailObject.style.borderColor = "red";
+      mailObject.style.borderColor = 'red';
       event.preventDefault();
     } else {
-      mailObject.style.borderColor = "rgb(255, 174, 0)";
+      mailObject.style.borderColor = '#529d44';
     }
 
     if (!mailContent || mailContent.value.length === 0) {
-      mailContent.style.borderColor = "red";
+      mailContent.style.borderColor = 'red';
       event.preventDefault();
     } else {
-      mailContent.style.borderColor = "rgb(255, 174, 0)";
+      mailContent.style.borderColor = '#529d44';
     }
   });
 });
