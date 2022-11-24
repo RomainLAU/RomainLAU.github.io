ce676af3be269f871d606225c7f69a2d6030f104
RomainLAU <romain.laurent@epitech.digital>
2022-11-24 23:59:59 +0100

    Attempts to fix blur effect on little menu and increase gap in the form

--- dropdown.js
+++ dropdown.js
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const burgerMenu = document.querySelector("#burger-button");
-  const littleMenu = document.querySelector("#little-menu");
-  const subMenu = document.querySelector("#sub-menu");
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerMenu = document.querySelector('#burger-button');
+  const littleMenu = document.querySelector('#little-menu');
+  const subMenu = document.querySelector('#sub-menu');
 
   burgerMenu.style.backgroundImage = "url('assets/images/bars.svg')";
 
-  littleMenu.addEventListener("click", () => {
-    if (subMenu.style.display == "block") {
+  littleMenu.addEventListener('click', () => {
+    if (subMenu.style.display == 'block') {
       burgerMenu.style.backgroundImage = "url('assets/images/bars.svg')";
-      subMenu.style.display = "none";
+      subMenu.style.display = 'none';
     } else {
-      subMenu.style.display = "block";
+      subMenu.style.display = 'block';
       burgerMenu.style.backgroundImage = "url('assets/images/xmark.svg')";
     }
   });
 
-  littleMenu.addEventListener("mouseleave", () => {
+  littleMenu.addEventListener('mouseleave', () => {
     burgerMenu.style.backgroundImage = "url('assets/images/bars.svg')";
-    subMenu.style.display = "none";
+    subMenu.style.display = 'none';
   });
 });
