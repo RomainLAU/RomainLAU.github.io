4aeb711e2c388c51f6ae39ca428ca7750f18318b
RomainLAU <romain.laurent@epitech.digital>
2023-11-20 01:03:39 +0100

    Add animation on the submenu

--- dropdown.js
+++ dropdown.js
@@ -1,37 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const burgerMenu = document.querySelector('#burger-button');
+  const burgerButton = document.querySelector('#burger-button');
   const littleMenu = document.querySelector('#little-menu');
   const subMenu = document.querySelector('#sub-menu');
 
-  burgerMenu.style.backgroundImage = "url('/assets/images/bars.svg')";
+  burgerButton.style.backgroundImage = "url('/assets/images/bars.svg')";
+  subMenu.style.display = 'none';
+
+  const removeSubmenu = () => {
+    burgerButton.style.backgroundImage = "url('/assets/images/bars.svg')";
+    subMenu.style.gridTemplateRows = 'repeat(4, 0fr)';
+    subMenu.style.opacity = '0';
+
+    setTimeout(() => {
+      subMenu.style.display = 'none';
+    }, 400);
+  };
 
   littleMenu.addEventListener('click', () => {
-    if (subMenu.style.display == 'block') {
-      burgerMenu.style.backgroundImage = "url('/assets/images/bars.svg')";
-      subMenu.style.display = 'none';
+    if (subMenu.style.opacity == '1') {
+      removeSubmenu();
     } else {
-      subMenu.style.display = 'block';
-      burgerMenu.style.backgroundImage = "url('/assets/images/xmark.svg')";
+      subMenu.style.display = 'grid';
+
+      setTimeout(() => {
+        subMenu.style.gridTemplateRows = 'repeat(4, 1fr)';
+        subMenu.style.opacity = '1';
+        burgerButton.style.backgroundImage = "url('/assets/images/xmark.svg')";
+      }, 10);
     }
   });
 
-  littleMenu.addEventListener('mouseleave', () => {
-    burgerMenu.style.backgroundImage = "url('/assets/images/bars.svg')";
-    subMenu.style.display = 'none';
-  });
+  littleMenu.addEventListener('mouseleave', removeSubmenu);
 
-  littleMenu.addEventListener('touchleave', () => {
-    burgerMenu.style.backgroundImage = "url('/assets/images/bars.svg')";
-    subMenu.style.display = 'none';
-  });
+  littleMenu.addEventListener('touchleave', removeSubmenu);
 
-  littleMenu.addEventListener('touchcancel', () => {
-    burgerMenu.style.backgroundImage = "url('/assets/images/bars.svg')";
-    subMenu.style.display = 'none';
-  });
+  littleMenu.addEventListener('touchcancel', removeSubmenu);
 
-  document.addEventListener('scroll', () => {
-    burgerMenu.style.backgroundImage = "url('/assets/images/bars.svg')";
-    subMenu.style.display = 'none';
-  });
+  document.addEventListener('scroll', removeSubmenu);
 });
