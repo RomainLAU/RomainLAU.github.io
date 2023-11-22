9851f6dddd0e36d0833608c9ecc0e9b5e57a77b2
RomainLAU <romain.laurent@epitech.digital>
2023-11-22 20:53:18 +0100

    update the animation of the dropdown menu

--- dropdown.js
+++ dropdown.js
@@ -4,29 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const subMenu = document.querySelector('#sub-menu');
 
   burgerButton.style.backgroundImage = "url('/assets/images/bars.svg')";
-  subMenu.style.display = 'none';
+  // subMenu.style.display = 'none';
 
   const removeSubmenu = () => {
     burgerButton.style.backgroundImage = "url('/assets/images/bars.svg')";
-    subMenu.style.gridTemplateRows = 'repeat(4, 0fr)';
-    subMenu.style.opacity = '0';
-
-    setTimeout(() => {
-      subMenu.style.display = 'none';
-    }, 401);
+    subMenu.style.transform = 'translateY(-100%)';
   };
 
   littleMenu.addEventListener('click', () => {
-    if (subMenu.style.opacity == '1') {
+    if (subMenu.style.transform == 'translateY(0%)') {
       removeSubmenu();
     } else {
-      subMenu.style.display = 'grid';
-
-      setTimeout(() => {
-        subMenu.style.gridTemplateRows = 'repeat(4, 1fr)';
-        subMenu.style.opacity = '1';
-        burgerButton.style.backgroundImage = "url('/assets/images/xmark.svg')";
-      }, 1);
+      subMenu.style.transform = 'translateY(0%)';
+      burgerButton.style.backgroundImage = "url('/assets/images/xmark.svg')";
     }
   });
 
